00fab3caa6a4d2669a45cc22265b833e3a94838e
akshay <akzfowl@akzfowl.(none)>
2014-05-31 22:41:18 +0530

    Done

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,19 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a <= 0 or b <= 0 or c <= 0 then
+    raise TriangleError
+  end
+  if (a+b) <= c or (b+c) <= a or (c+a) <= b
+    raise TriangleError
+  end
+  if a == b and a == c then
+    :equilateral
+  elsif a == b or b == c or c == a then
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
